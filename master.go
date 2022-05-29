8f841488080e085d9270a93293d6325091874ad2
John scherbatsky <john.scherbatsky@gmail.com>
2022-05-29 19:13:46 +0800

    калькулятор 4х операций

---
+++ master.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+	var oprnd1, oprnd2, result float32
+	var operator string
+
+	fmt.Print("введите первое число: ")
+	_, err := fmt.Scanln(&oprnd1)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Число не прочитано %s", err))
+
+	}
+	// fmt.Println(oprnd1)
+
+	fmt.Print("укажите операцию: (+,-,*,/) ")
+	_, err = fmt.Scanln(&operator)
+	if err != nil { // не знаю как реализовать проверку на корректный ввод операции
+		fmt.Println(fmt.Sprintf("некорректный формат %s", err))
+		if operator != "+, -, *, /" {
+			fmt.Println("oshibka")
+		}
+	}
+
+	// fmt.Println(operator)
+
+	fmt.Print("введите второе число: ")
+	_, err = fmt.Scanln(&oprnd2)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Число не прочитано %s", err))
+	}
+	// fmt.Println(oprnd2)
+
+	switch operator {
+	case "+":
+		result = oprnd1 + oprnd2
+		fmt.Println("Результат суммы: ", result)
+
+	case "-":
+		result = oprnd1 - oprnd2
+		fmt.Println("Результат разности: ", result)
+
+	case "*":
+		result = oprnd1 * oprnd2
+		fmt.Println("Результат умножения: ", result)
+
+	case "/":
+		if oprnd2 == 0 { // проверим делимое на условие не равности нулю
+			fmt.Println("Не делим на ноль")
+		} else {
+			result = oprnd1 / oprnd2
+			fmt.Println("Результат частного: ", result)
+		}
+
+	default:
+		fmt.Println("Не указана запрашиваемая операция")
+
+	}
+
+}
